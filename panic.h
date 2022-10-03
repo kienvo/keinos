ca818b86f1a0d102c1ae2139be6ad3957c532e1a
kienvo <kein@kienlab.com>
2022-10-03 10:30:08 +0700

    panic: add PANIC macro

--- panic.h
+++ panic.h
@@ -1,6 +1,7 @@
 #ifndef __PANIC_H__
 #define __PANIC_H__
 
+#define PANIC(fmt, ...) panic("%s:%d: " fmt,__FILE__, __LINE__, ##__VA_ARGS__)
 void panic(const char *fmt, ...);
 
 #endif /* __PANIC_H__ */
